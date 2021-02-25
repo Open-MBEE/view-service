f8d0c523b85eab6d84abc2f7d2919203ccc04da4
Blake Regalia <blake.d.regalia@jpl.nasa.gov>
2021-02-25 12:57:05 -0800

    style: move async into promise.then

--- src/functions.js
+++ src/functions.js
@@ -103,16 +103,16 @@ export async function popConfluencePageTree(event) {
     };
 }
 
-export async function generateUuid() {
-    return uuidv4();
+export function generateUuid() {
+    return Promise.resolve(uuidv4());
 }
 
-export async function flattenArray(event) {
+export function flattenArray(event) {
     const isEventArray = Array.isArray(event);
     if(!isEventArray && !Array.isArray(event?.args)) {
         throw new Error('Event must be an array or an object with an `args` key that is an array');
     }
-    return (isEventArray ? event : event.args).flat();
+    return Promise.resolve((isEventArray ? event : event.args).flat());
 }
 
 export function copyS3Object(event) {
